6e8ca817d7bf0e64ef4ad3f058be047a4336a655
John Rivers <jrivers@nmsu.edu>
2018-02-19 22:43:25 -0700

    Added phone functionality and minor tweaks.

--- app/src/main/java/edu/nmsu/cs/lcpdandu/ContactUs.java
+++ app/src/main/java/edu/nmsu/cs/lcpdandu/ContactUs.java
@@ -1,7 +1,10 @@
 package edu.nmsu.cs.lcpdandu;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,11 +22,43 @@ public class ContactUs extends AppCompatActivity {
         startActivity(new Intent(ContactUs.this, HomePage.class));
     }
 
-    public void clickFacebook(View v) {
+    public void clickPoliceWebsite(View v) {
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_VIEW);
         intent.addCategory(Intent.CATEGORY_BROWSABLE);
         intent.setData(Uri.parse("http://www.las-cruces.org/departments/police-department"));
         startActivity(intent);
+    }
+
+    public void clickPoliceFacebook(View v) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("https://www.facebook.com/TheLasCrucesPoliceDepartment"));
+        startActivity(intent);
+    }
+
+    public void clickPoliceTwitter(View v) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("https://twitter.com/lascrucespolice"));
+        startActivity(intent);
+    }
+
+    public void callPhone(View V) {
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:5756400344"));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(callIntent);
     }
 }
