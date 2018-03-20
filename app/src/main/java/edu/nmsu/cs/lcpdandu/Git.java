4b7b85b4d5a81eb2a513ec37cd70b32e778ea9a5
John Rivers <jrivers@nmsu.edu>
2018-03-19 20:47:23 -0600

    Notifications activity now GETs all notifications using REST. Output is not fully formatted.

---
+++ app/src/main/java/edu/nmsu/cs/lcpdandu/Git.java
@@ -0,0 +1,124 @@
+package edu.nmsu.cs.lcpdandu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.Response;
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+public class Git extends AppCompatActivity {
+
+    EditText etGitHubUser; // This will be a reference to our GitHub username input.
+    Button btnGetRepos;  // This is a reference to the "Get Repos" button.
+    TextView tvRepoList;  // This will reference our repo list text box.
+    RequestQueue requestQueue;  // This is our requests queue to process our HTTP requests.
+
+    String baseUrl = "https://api.github.com/users/";  // This is the API base URL (GitHub API)
+    String url;  // This will hold the full URL which will include the username entered in the etGitHubUser.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);  // This is some magic for Android to load a previously saved state for when you are switching between actvities.
+        setContentView(R.layout.activity_git);  // This links our code to our layout which we defined earlier.
+
+        this.etGitHubUser = (EditText) findViewById(R.id.et_github_user);  // Link our github user text box.
+        this.btnGetRepos = (Button) findViewById(R.id.btn_get_repos);  // Link our clicky button.
+        this.tvRepoList = (TextView) findViewById(R.id.tv_repo_list);  // Link our repository list text output box.
+        this.tvRepoList.setMovementMethod(new ScrollingMovementMethod());  // This makes our text box scrollable, for those big GitHub contributors with lots of repos :)
+
+        requestQueue = Volley.newRequestQueue(this);  // This setups up a new request queue which we will need to make HTTP requests.
+    }
+
+    private void clearRepoList() {
+        // This will clear the repo list (set it as a blank string).
+        this.tvRepoList.setText("");
+    }
+
+    private void addToRepoList(String repoName, String lastUpdated) {
+        // This will add a new repo to our list.
+        // It combines the repoName and lastUpdated strings together.
+        // And then adds them followed by a new line (\n\n make two new lines).
+        String strRow = repoName + " / " + lastUpdated;
+        String currentText = tvRepoList.getText().toString();
+        this.tvRepoList.setText(currentText + "\n\n" + strRow);
+    }
+
+    private void setRepoListText(String str) {
+        // This is used for setting the text of our repo list box to a specific string.
+        // We will use this to write a "No repos found" message if the user doens't have any.
+        this.tvRepoList.setText(str);
+    }
+
+    private void getRepoList(String username) {
+        // First, we insert the username into the repo url.
+        // The repo url is defined in GitHubs API docs (https://developer.github.com/v3/repos/).
+        this.url = this.baseUrl + username + "/repos";
+
+        // Next, we create a new JsonArrayRequest. This will use Volley to make a HTTP request
+        // that expects a JSON Array Response.
+        // To fully understand this, I'd recommend readng the office docs: https://developer.android.com/training/volley/index.html
+        JsonArrayRequest arrReq = new JsonArrayRequest(Request.Method.GET, url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // Check the length of our response (to see if the user has any repos)
+                        if (response.length() > 0) {
+                            // The user does have repos, so let's loop through them all.
+                            for (int i = 0; i < response.length(); i++) {
+                                try {
+                                    // For each repo, add a new line to our repo list.
+                                    JSONObject jsonObj = response.getJSONObject(i);
+                                    String repoName = jsonObj.get("name").toString();
+                                    String lastUpdated = jsonObj.get("updated_at").toString();
+                                    addToRepoList(repoName, lastUpdated);
+                                } catch (JSONException e) {
+                                    // If there is an error then output this to the logs.
+                                    Log.e("Volley", "Invalid JSON Object.");
+                                }
+
+                            }
+                        } else {
+                            // The user didn't have any repos.
+                            setRepoListText("No repos found.");
+                        }
+
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // If there a HTTP error then add a note to our repo list.
+                        setRepoListText("Error while calling REST API");
+                        Log.e("Volley", error.toString());
+                    }
+                }
+        );
+        // Add the request we just defined to our request queue.
+        // The request queue will automatically handle the request as soon as it can.
+        requestQueue.add(arrReq);
+    }
+
+    public void getReposClicked(View v) {
+        // Clear the repo list (so we have a fresh screen to add to)
+        clearRepoList();
+        // Call our getRepoList() function that is defined above and pass in the
+        // text which has been entered into the etGitHubUser text input field.
+        getRepoList(etGitHubUser.getText().toString());
+    }
+
+}
--- app/src/main/java/edu/nmsu/cs/lcpdandu/Notifications.java
+++ app/src/main/java/edu/nmsu/cs/lcpdandu/Notifications.java
@@ -4,9 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
@@ -22,69 +19,57 @@ import org.json.JSONException;
 
 public class Notifications extends AppCompatActivity {
 
-    EditText etGitHubUser; // This will be a reference to our GitHub username input.
-    Button btnGetRepos;  // This is a reference to the "Get Repos" button.
-    TextView tvRepoList;  // This will reference our repo list text box.
+    TextView tvNotificationList;  // This will reference our notification list text box.
     RequestQueue requestQueue;  // This is our requests queue to process our HTTP requests.
 
-    String baseUrl = "https://api.github.com/users/";  // This is the API base URL (GitHub API)
-    String url;  // This will hold the full URL which will include the username entered in the etGitHubUser.
+    String url = "http://tm4sp18.cs.nmsu.edu:8000/public/api/notifications";  // This is the API base URL (GitHub API)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);  // This is some magic for Android to load a previously saved state for when you are switching between actvities.
         setContentView(R.layout.activity_notifications);  // This links our code to our layout which we defined earlier.
 
-        this.etGitHubUser = (EditText) findViewById(R.id.et_github_user);  // Link our github user text box.
-        this.btnGetRepos = (Button) findViewById(R.id.btn_get_repos);  // Link our clicky button.
-        this.tvRepoList = (TextView) findViewById(R.id.tv_repo_list);  // Link our repository list text output box.
-        this.tvRepoList.setMovementMethod(new ScrollingMovementMethod());  // This makes our text box scrollable, for those big GitHub contributors with lots of repos :)
+        this.tvNotificationList = (TextView) findViewById(R.id.tv_notification_list);  // Link our notificationsitory list text output box.
+        this.tvNotificationList.setMovementMethod(new ScrollingMovementMethod());  // This makes our text box scrollable, for those big GitHub contributors with lots of notifications :)
 
         requestQueue = Volley.newRequestQueue(this);  // This setups up a new request queue which we will need to make HTTP requests.
+
+        getNotificationList();
     }
 
-    private void clearRepoList() {
-        // This will clear the repo list (set it as a blank string).
-        this.tvRepoList.setText("");
+    private void addToNotificationList(String notificationID, String notificationTitle, String notificationDescription, String notificationDate, String notificationTime, String notificationAMPM) {
+        String strRow = notificationID + "\n" + notificationTitle + "\n" + notificationDescription + "\n" + notificationDate + "\n" + notificationTime + " " + notificationAMPM;
+        String currentText = tvNotificationList.getText().toString();
+        this.tvNotificationList.setText(currentText + "\n\n" + strRow);
     }
 
-    private void addToRepoList(String repoName, String lastUpdated) {
-        // This will add a new repo to our list.
-        // It combines the repoName and lastUpdated strings together.
-        // And then adds them followed by a new line (\n\n make two new lines).
-        String strRow = repoName + " / " + lastUpdated;
-        String currentText = tvRepoList.getText().toString();
-        this.tvRepoList.setText(currentText + "\n\n" + strRow);
+    private void setNotificationListText(String str) {
+
+        this.tvNotificationList.setText(str);
     }
 
-    private void setRepoListText(String str) {
-        // This is used for setting the text of our repo list box to a specific string.
-        // We will use this to write a "No repos found" message if the user doens't have any.
-        this.tvRepoList.setText(str);
-    }
+    private void getNotificationList() {
 
-    private void getRepoList(String username) {
-        // First, we insert the username into the repo url.
-        // The repo url is defined in GitHubs API docs (https://developer.github.com/v3/repos/).
-        this.url = this.baseUrl + username + "/repos";
-
-        // Next, we create a new JsonArrayRequest. This will use Volley to make a HTTP request
-        // that expects a JSON Array Response.
-        // To fully understand this, I'd recommend readng the office docs: https://developer.android.com/training/volley/index.html
+        // Creates a new JsonArrayRequest.
+        // Uses Volley to make a HTTP request that expects a JSON Array Response.
         JsonArrayRequest arrReq = new JsonArrayRequest(Request.Method.GET, url,
                 new Response.Listener<JSONArray>() {
                     @Override
                     public void onResponse(JSONArray response) {
-                        // Check the length of our response (to see if the user has any repos)
+                        // Check the length of our response (to see if the user has any notifications)
                         if (response.length() > 0) {
-                            // The user does have repos, so let's loop through them all.
+                            // The user does have notifications, so let's loop through them all.
                             for (int i = 0; i < response.length(); i++) {
                                 try {
-                                    // For each repo, add a new line to our repo list.
+                                    // For each notification, add a new line to our notification list.
                                     JSONObject jsonObj = response.getJSONObject(i);
-                                    String repoName = jsonObj.get("name").toString();
-                                    String lastUpdated = jsonObj.get("updated_at").toString();
-                                    addToRepoList(repoName, lastUpdated);
+                                    String notificationID = jsonObj.get("ID").toString();
+                                    String notificationTitle = jsonObj.get("Title").toString();
+                                    String notificationDescription = jsonObj.get("Description").toString();
+                                    String notificationDate = jsonObj.get("PostDate").toString();
+                                    String notificationTime = jsonObj.get("PostTime").toString();
+                                    String notificationAMPM = jsonObj.get("PostTimeAMPM").toString();
+                                    addToNotificationList(notificationID, notificationTitle, notificationDescription, notificationDate, notificationTime, notificationAMPM);
                                 } catch (JSONException e) {
                                     // If there is an error then output this to the logs.
                                     Log.e("Volley", "Invalid JSON Object.");
@@ -92,8 +77,8 @@ public class Notifications extends AppCompatActivity {
 
                             }
                         } else {
-                            // The user didn't have any repos.
-                            setRepoListText("No repos found.");
+                            // The user didn't have any notifications.
+                            setNotificationListText("No notifications found.");
                         }
 
                     }
@@ -102,8 +87,8 @@ public class Notifications extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        // If there a HTTP error then add a note to our repo list.
-                        setRepoListText("Error while calling REST API");
+                        // If there a HTTP error then add a note to our notification list.
+                        setNotificationListText("Error while calling REST API");
                         Log.e("Volley", error.toString());
                     }
                 }
@@ -111,14 +96,6 @@ public class Notifications extends AppCompatActivity {
         // Add the request we just defined to our request queue.
         // The request queue will automatically handle the request as soon as it can.
         requestQueue.add(arrReq);
-    }
-
-    public void getReposClicked(View v) {
-        // Clear the repo list (so we have a fresh screen to add to)
-        clearRepoList();
-        // Call our getRepoList() function that is defined above and pass in the
-        // text which has been entered into the etGitHubUser text input field.
-        getRepoList(etGitHubUser.getText().toString());
     }
 
 }
