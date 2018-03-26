025e28bf2cf23408e2ff5c1278b9cf9160d8852d
Destoni Baldwin <tonibwin@hotmail.com>
2018-03-26 08:55:20 -0600

    Notifications are in List format now. Clickable but do nothing

---
+++ app/src/main/java/edu/nmsu/cs/lcpdandu/NotificationObjects.java
@@ -0,0 +1,23 @@
+package edu.nmsu.cs.lcpdandu;
+
+/**
+ * Created by tonib on 3/26/2018.
+ */
+
+public class NotificationObjects {
+
+    public String ID;
+    public String Title;
+    public String Description;
+    public String Date;
+    public String Time;
+    public String AMPM;
+
+    public NotificationObjects(){
+        String ID = this.ID;
+        String Title = this.Title;
+        String Description = this.Description;
+        String Time = this.Time;
+        String AMPM = this.AMPM;
+    }
+}
