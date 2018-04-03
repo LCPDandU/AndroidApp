bba1637c1b6ba62e22be673868fdec1018c2ebc1
John Rivers <jrivers@nmsu.edu>
2018-04-02 21:23:27 -0600

    Added first test for Android app files for push notifications.

---
+++ app/src/main/java/edu/nmsu/cs/lcpdandu/Token.java
@@ -0,0 +1,44 @@
+package edu.nmsu.cs.lcpdandu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Token extends AppCompatActivity implements View.OnClickListener {
+
+    //defining views
+    private Button buttonDisplayToken;
+    private TextView textViewToken;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_token);
+
+        //getting views from xml
+        textViewToken = (TextView) findViewById(R.id.textViewToken);
+        buttonDisplayToken = (Button) findViewById(R.id.buttonDisplayToken);
+
+        //adding listener to view
+        buttonDisplayToken.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonDisplayToken) {
+            //getting token from shared preferences
+            String token = SharedPrefManager.getInstance(this).getDeviceToken();
+
+            //if token is not null
+            if (token != null) {
+                //displaying the token
+                textViewToken.setText(token);
+            } else {
+                //if token is null that means something wrong
+                textViewToken.setText("Token not generated");
+            }
+        }
+    }
+}
