cb9d7adba36ef7f72eec200f0401ad5a21ae37fc
Shawnzye <reavess7799@gmail.com>
2018-04-04 13:15:24 -0600

    EventObject

---
+++ app/src/main/java/edu/nmsu/cs/lcpdandu/EventObjects.java
@@ -0,0 +1,26 @@
+package edu.nmsu.cs.lcpdandu;
+
+public class EventObjects {
+    public String ID;
+    public String Title;
+    public String Category;
+    public String Date;
+    public String Time;
+    public String AMPM;
+    public String Location;
+    public String Description;
+
+
+    public EventObjects(){
+        String ID = this.ID;
+        String Title = this.Title;
+        String Category = this.Category;
+        String Date = this.Date;
+        String Description = this.Description;
+        String Time = this.Time;
+        String AMPM = this.AMPM;
+        String Location = this.Location;
+    }
+
+
+}
