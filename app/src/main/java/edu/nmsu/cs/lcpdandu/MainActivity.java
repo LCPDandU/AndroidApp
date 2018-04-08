00f7f37be3c39fa7b49e333d0af8d0310b34a0dc
John Rivers <jrivers@nmsu.edu>
2018-04-07 21:57:52 -0600

    Push notifications are working.

---
+++ app/src/main/java/edu/nmsu/cs/lcpdandu/MainActivity.java
@@ -0,0 +1,52 @@
+package edu.nmsu.cs.lcpdandu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvNotificationDetails;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initControls();
+
+        setNotificationData(getIntent().getExtras());
+    }
+
+    private void initControls() {
+
+        tvNotificationDetails = (TextView) findViewById(R.id.tvNotificationDetails);
+
+    }
+
+    private void setNotificationData(Bundle extras) {
+
+        if (extras == null)
+            return;
+
+        StringBuilder text = new StringBuilder("");
+
+        text.append("Message Details:");
+        text.append("\n");
+        text.append("\n");
+
+        if (extras.containsKey("title")) {
+            text.append("Title: ");
+            text.append(extras.get("title"));
+        }
+
+        text.append("\n");
+
+        if (extras.containsKey("message")) {
+            text.append("Message: ");
+            text.append(extras.get("message"));
+        }
+
+        tvNotificationDetails.setText(text);
+    }
+}
