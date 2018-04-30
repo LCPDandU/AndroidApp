5de891aca60d3f3d9e3aa395b18988a2dae74ce9
Shawnzye <reavess7799@gmail.com>
2018-04-30 01:39:29 -0600

    Added more test and Call button

--- app/src/androidTest/java/edu/nmsu/cs/lcpdandu/HomePageTest.java
+++ app/src/androidTest/java/edu/nmsu/cs/lcpdandu/HomePageTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.openLink;
+import static android.support.test.espresso.action.ViewActions.openLinkWithText;
 import static android.support.test.espresso.action.ViewActions.pressBack;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.action.ViewActions.swipeRight;
@@ -43,4 +45,49 @@ public class HomePageTest {
         onView(withId(R.id.navigation_notifications)).perform(click());
     }
 
+    @Test
+    public void askTheCity(){
+        onView(withId(R.id.navigation_navigation_ask_city)).perform(click());
+    }
+
+    @Test
+    public void eventBack(){
+        onView(withId(R.id.navigation_events)).perform(click());
+        onView(withId(R.id.prev_button)).perform(click());
+    }
+
+    @Test
+    public void eventNext(){
+        onView(withId(R.id.navigation_events)).perform(click());
+        onView(withId(R.id.next_button)).perform(click());
+    }
+
+    @Test
+    public void twitterButton(){
+        onView(withId(R.id.TwitterButton)).perform(click());
+    }
+
+    @Test
+    public void facebookButton(){
+        onView(withId(R.id.FacebookButton)).perform(click());
+    }
+
+    @Test
+    public void cityOfLasCruces(){
+        onView(withId(R.id.contact_social_media)).perform(click());
+    }
+
+    @Test
+    public void testClickFromEachNavigation(){
+        onView(withId(R.id.navigation_home)).perform(click());
+        onView(withId(R.id.navigation_events)).perform(click());
+        onView(withId(R.id.navigation_notifications)).perform(click());
+        onView(withId(R.id.navigation_contact)).perform(click());
+        onView(withId(R.id.navigation_navigation_ask_city)).perform(click());
+    }
+
+    @Test
+    public void callButton(){
+        onView(withId(R.id.PhoneButton)).perform(click());
+    }
 }
